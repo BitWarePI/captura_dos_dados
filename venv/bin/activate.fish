9ecc9284d44ac149e4f1a54b5cfd9a3fb2a3dd3d
NicolasBJaved <nicolas.javed@sptech.school>
2025-10-07 19:33:25 -0300

    Alteração no script para pegar dados da CPU e GPU com temperatura

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/lukascomka/Área de trabalho/python_script_Sprint1/venv'
+set -gx VIRTUAL_ENV /home/nicolas/sptech/calculo/script-python-bitware/captura_dos_dados/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
